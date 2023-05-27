cf34d17d9b1784c92c03bf732273ae29c274a192
sathit152538 <134771716+sathit152538@users.noreply.github.com>
2023-05-27 15:34:58 +0700

    Add files via upload

---
+++ sort_object.py
@@ -0,0 +1,11 @@
+from operator import itemgetter
+
+obj_detected = {'bottle': [110, 30], 'glass': [60, 35], 'book': [310, 23], 'phone': [260, 33], 'dice': [155, 38],
+                'mouse': [200, 45], 'keyboard': [298, 65], 'fan': [250, 36]}
+
+get_x_axis = itemgetter(1, 0)
+
+sorted_items = sorted(obj_detected.items(), key=get_x_axis, reverse=True)
+
+for item in (sorted_items):
+    print(item[0], item[1])
